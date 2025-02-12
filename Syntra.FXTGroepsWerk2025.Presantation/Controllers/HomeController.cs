45166c3ccea97386bef30f05007d454e98fcbb1f
TimoTimitoe <waelkenstimothy@gmail.com>
2025-02-12 14:26:12 +0100

    addded view for method for movie and book in home controller
    added empty views
    
    edited index vieuw to login view
    
    edited _layout

--- Syntra.FXTGroepsWerk2025.Presantation/Controllers/HomeController.cs
+++ Syntra.FXTGroepsWerk2025.Presantation/Controllers/HomeController.cs
@@ -13,15 +13,47 @@ namespace Syntra.FXTGroepsWerk2025.Presantation.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Method to view the main index
+        /// </summary>
+        /// <returns> returns view: "Index"</returns>
         public IActionResult Index()
+        
         {
             return View();
         }
 
+        /// <summary>
+        /// Method to view privecy page
+        /// </summary>
+        /// <returns> returns view: "Index"</returns>
         public IActionResult Privacy()
         {
             return View();
         }
+
+        /// <summary>
+        /// Method to view movies wishlist
+        /// </summary>
+        /// <returns>returns view : "Movie"</returns>
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// Method to view books wishlist
+        /// </summary>
+        /// <returns>returns view: "Books"</returns>
+        public IActionResult Books()
+        {
+            return View();
+        }
+
+        //public IActionResult Favorites()
+        //{
+        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
