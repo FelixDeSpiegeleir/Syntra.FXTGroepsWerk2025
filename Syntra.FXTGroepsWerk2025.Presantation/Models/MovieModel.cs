28063c853fe15ae2cfa563788d0f756c87d6a093
TimoTimitoe <waelkenstimothy@gmail.com>
2025-02-16 15:47:20 +0100

    made moviemodel

--- Syntra.FXTGroepsWerk2025.Presantation/Models/MovieModel.cs
+++ Syntra.FXTGroepsWerk2025.Presantation/Models/MovieModel.cs
@@ -2,5 +2,23 @@
 {
     public class MovieModel
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+
+        public MovieModel() { }
+
+        public MovieModel(int id, string title, string director, DateTime releaseDate, string genre, double rating)
+        {
+            Id = id;
+            Title = title;
+            Director = director;
+            ReleaseDate = releaseDate;
+            Genre = genre;
+            Rating = rating;
+        }
     }
 }
