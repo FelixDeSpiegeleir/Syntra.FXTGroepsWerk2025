6e31e56770b5a5e33b6410a4b5369a64ac44d351
TimoTimitoe <waelkenstimothy@gmail.com>
2025-02-16 15:56:14 +0100

    removed temp views , added moviemodel

--- Syntra.FXTGroepsWerk2025.Presantation/Models/MovieModel.cs
+++ Syntra.FXTGroepsWerk2025.Presantation/Models/MovieModel.cs
@@ -1,24 +1,38 @@
-﻿namespace Syntra.FXTGroepsWerk2025.Presantation.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OWN.GroupProject2.Objects
 {
-    public class MovieModel
+    /// <summary>
+    /// Represents a Movie entity, inheriting from WatchListItem.
+    /// </summary>
+    public class Movie : WatchListItem
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Director { get; set; }
-        public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
-        public double Rating { get; set; }
+        /// <summary>
+        /// Gets or sets the duration of the movie in minutes.
+        /// </summary>
+        [Required(ErrorMessage = "Duration is required.")]
+        [Range(1, 600, ErrorMessage = "Duration must be between 1 and 600 minutes.")]
+        public int Duration { get; set; }
 
-        public MovieModel() { }
+        /// <summary>
+        /// Gets or sets the year the movie was released.
+        /// </summary>
+        [Required(ErrorMessage = "Release year is required.")]
+        [Range(1888, 2100, ErrorMessage = "Please enter a valid year.")] // 1888: First known movie
+        public int Year { get; set; }
 
-        public MovieModel(int id, string title, string director, DateTime releaseDate, string genre, double rating)
-        {
-            Id = id;
-            Title = title;
-            Director = director;
-            ReleaseDate = releaseDate;
-            Genre = genre;
-            Rating = rating;
-        }
+        /// <summary>
+        /// Gets or sets the director of the movie.
+        /// </summary>
+        [Required(ErrorMessage = "Director is required.")]
+        public virtual Director Director { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unique identifier for the director in the database.
+        /// </summary>
+        [ForeignKey("Director")]
+        public int DirectorId { get; set; }
     }
 }
